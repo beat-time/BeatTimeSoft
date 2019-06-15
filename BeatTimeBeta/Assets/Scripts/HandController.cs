db4cf8b91492a653bd50fef55df085f27f663a76
valentin <valentin@gmail.com>
2019-06-15 16:49:09 -0500

    handController
    prueb de movimiento de manos

---
+++ BeatTimeBeta/Assets/Scripts/HandController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    private GameObject RightHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //is right hand is empty null it finding else it changing hand position
+        if(RightHand == null)
+        {
+            RightHand = GameObject.Find("HandRight");
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(-RightHand.transform.position.x, RightHand.transform.position.y, transform.position.z);
+        }
+    }
+}
