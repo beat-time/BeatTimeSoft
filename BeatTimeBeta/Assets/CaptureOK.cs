5fb35d7d7d71d652c79ef92671949a1939d36fe7
DESKTOP-MKELDPC\Anthony Anca <tony_fnx@hotmail.com>
2019-06-27 01:37:49 -0500

    Certificación Spring 3
    Avance hasta la certificación del Spring 3

---
+++ BeatTimeBeta/Assets/CaptureOK.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CaptureOK : MonoBehaviour
+{
+    public KinectUICursor CursorRigth;
+ 
+    public Button btnok;
+    public Button btnBack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CursorRigth.transform.position.x > btnok.transform.position.x - 50 && CursorRigth.transform.position.x < btnok.transform.position.x + 50 && CursorRigth.transform.position.y > btnok.transform.position.y - 50 && CursorRigth.transform.position.y < btnok.transform.position.y + 50)
+        {
+            ChangeScene();
+        }
+
+        if (CursorRigth.transform.position.x > btnBack.transform.position.x - 40 && CursorRigth.transform.position.x < btnBack.transform.position.x + 40 && CursorRigth.transform.position.y > btnBack.transform.position.y - 40 && CursorRigth.transform.position.y < btnBack.transform.position.y + 40)
+        {
+            ChangeBack();
+        }
+
+    }
+
+    public void ChangeScene()
+    {
+        if (CursorRigth.HandGreen == true)
+        {
+            SceneManager.LoadScene("MyPhotoP2");
+            //contPlayers.Players = 1;
+        }
+    }
+    public void ChangeBack()
+    {
+        if (CursorRigth.HandGreen == true)
+        {
+            SceneManager.LoadScene("SelecPlayers");
+            //contPlayers.Players = 1;
+        }
+    }
+}
---
+++ BeatTimeBeta/Assets/SelectSongChance.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SelectSongChance : MonoBehaviour
+{
+    public KinectUICursor CursorRigth;
+
+    public Button btnok;
+    public Button btnBack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CursorRigth.transform.position.x > btnok.transform.position.x - 50 && CursorRigth.transform.position.x < btnok.transform.position.x + 50 && CursorRigth.transform.position.y > btnok.transform.position.y - 50 && CursorRigth.transform.position.y < btnok.transform.position.y + 50)
+        {
+            ChangeScene();
+        }
+
+        if (CursorRigth.transform.position.x > btnBack.transform.position.x - 50 && CursorRigth.transform.position.x < btnBack.transform.position.x + 50 && CursorRigth.transform.position.y > btnBack.transform.position.y - 50 && CursorRigth.transform.position.y < btnBack.transform.position.y + 50)
+        {
+            ChangeBack();
+        }
+    }
+
+    public void ChangeScene()
+    {
+        if (CursorRigth.HandGreen == true)
+        {
+            SceneManager.LoadScene("SelecThemes");
+            //contPlayers.Players = 1;
+        }
+    }
+    public void ChangeBack()
+    {
+        if (CursorRigth.HandGreen == true)
+        {
+            SceneManager.LoadScene("MyPhotoP1");
+            //contPlayers.Players = 1;
+        }
+    }
+}
