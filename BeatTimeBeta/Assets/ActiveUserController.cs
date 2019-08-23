a9132d7037b50c3fc087b85fdbbc8882678a97c6
EielsonJ <42283903+EielsonJ@users.noreply.github.com>
2019-08-23 17:51:00 -0500

    Contadores entre escenarios
    Si el kinect no detecta a un usuario en un intervalo de tiempo, automáticamente retorna a la escena principal.

---
+++ BeatTimeBeta/Assets/ActiveUserController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActiveUserController : MonoBehaviour
+{
+    public BodySourceView bodySourceView;
+    public float time;
+    float totalTime = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = totalTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!bodySourceView.Active)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+        }
+        else
+        {
+            time = totalTime;
+        }
+    }
+}
