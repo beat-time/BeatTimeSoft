450849413e5e538077d5c3402f1e74af141e5031
EielsonJ <42283903+EielsonJ@users.noreply.github.com>
2019-08-26 20:18:36 -0500

    Carrusel Parte 2
    Se mejoró la vista de listado de canciones, se reemplazó las imágenes de los laterales con flechas, se cambió la imagen de fondo, se eliminó los botones laterales y se cambió el diseño del botón "START". Asimismo, se colocó un gif de movimiento de manos a modo de tutorial para el usuario con duración de 5 segundos.

--- BeatTimeBeta/Assets/ChangeVideo.cs
+++ BeatTimeBeta/Assets/ChangeVideo.cs
@@ -11,15 +11,13 @@ public class ChangeVideo : MonoBehaviour
     public Button BtnStart;
     public int contador = 1;
     int maxContador = 5;
-    public bool slide = false;
-    public RawImage imageLeft;
+  
+
     public RawImage imageCenter;
-    public RawImage imageRight;
+    
     VideoPlayer video_player_center;
     AudioSource audio_source_center;
 
-    VideoPlayer video_player_left;
-    VideoPlayer video_player_right;
 
     //Videos/Music
     public VideoPlayer v1;
@@ -34,27 +32,11 @@ public class ChangeVideo : MonoBehaviour
     public AudioSource m4;
     public AudioSource m5;
 
-    public RenderTexture rt1;
-    public RenderTexture rt2;
-    public RenderTexture rt3;
 
-    float widthLeft;
-    float heightLeft;
-    float widthRight;
-    float heightRight;
-    float widthCenter;
-    float heightCenter;
-    float posXLeft;
-    float posYLeft;
-    float posXRight;
-    float posYRight;
-    float posXCenter;
-    float posYCenter;
-
-    public float crHeight;
-    public float crWidth;
-    public float clHeight;
-    public float clWidth;
+    float crHeight;
+    float crWidth;
+    float clHeight;
+    float clWidth;
 
     float Rinicio = 0.0f;
     float Rfin = 0.0f;
@@ -71,37 +53,29 @@ public class ChangeVideo : MonoBehaviour
 
     float btnPosX = 0.0f;
     float btnPosY = 0.0f;
+
+    float timeTutorial = 10;
+    public RawImage HandRightTutorial;
+    public RawImage HandLeftTutorial;
+
+    public RectTransform rectTransform;
+    float ImageCenterWidth;
+    float ImageCenterHeight;
+    bool change = false;
+    bool newVideo = false;
+
+    public RenderTexture renderTexture;
     // Start is called before the first frame update
     void Start()
     {
-        rt1.Release();
-
-        video_player_left = imageLeft.GetComponent<VideoPlayer>();
-        video_player_right = imageRight.GetComponent<VideoPlayer>();
         video_player_center = imageCenter.GetComponent<VideoPlayer>();
         audio_source_center = imageCenter.GetComponent<AudioSource>();
 
 
         video_player_center.clip = v1.clip;
-        video_player_right.clip = v2.clip;
-        video_player_right.Play();
-        video_player_right.Pause();
         video_player_center.Play();
         audio_source_center = m1;
         audio_source_center.Play();
-
-        widthLeft = imageLeft.rectTransform.rect.width;
-        heightLeft = imageLeft.rectTransform.rect.height;
-        widthRight = imageRight.rectTransform.rect.width;
-        heightRight = imageRight.rectTransform.rect.height;
-        widthCenter = imageCenter.rectTransform.rect.width;
-        heightCenter = imageCenter.rectTransform.rect.height;
-        posXLeft = imageLeft.transform.position.x;
-        posYLeft = imageLeft.transform.position.y;
-        posXRight = imageRight.transform.position.x;
-        posYRight = imageRight.transform.position.y;
-        posXCenter = imageCenter.transform.position.x;
-        posYCenter = imageCenter.transform.position.y;
 
         crHeight = CursorRight.GetComponent<RectTransform>().rect.height;
         crWidth = CursorRight.GetComponent<RectTransform>().rect.width;
@@ -112,71 +86,128 @@ public class ChangeVideo : MonoBehaviour
         btnHeight = BtnStart.GetComponent<RectTransform>().rect.width;
         btnPosX = BtnStart.transform.position.x;
         btnPosY = BtnStart.transform.position.y;
+
+        rectTransform = imageCenter.GetComponent<RectTransform>();
+        ImageCenterWidth = rectTransform.rect.width;
+        ImageCenterHeight = rectTransform.rect.height;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CursorRight.HandGreen)
-        {
-            if (!RbInicio)
-            {
-                Rinicio = CursorRight.transform.position.x;
-                RbInicio = true;
-            }
-        }
-        else if (!CursorRight.HandGreen)
-        {
-            if (RbInicio)
-            {
-                Rfin = CursorRight.transform.position.x;
-                if (Rinicio - Rfin > 100)
-                {
-                    Change_Next();
-                }
-                RbInicio = false;
-            }
-        }
-        if (CursorLeft.HandGreen)
-        {
-            if (!LbInicio)
-            {
-                Linicio = CursorLeft.transform.position.x;
-                LbInicio = true;
-            }
-        }
-        else if (!CursorLeft.HandGreen)
-        {
-            if (LbInicio)
-            {
-                Lfin = CursorLeft.transform.position.x;
-                if (Lfin - Linicio > 100)
-                {
-                    Change_Before();
-                }
-                LbInicio = false;
-            }
-        }
         Click_Start();
         Update_All();
+
+        CancelTutorial();
+
+        if (change)
+        {
+            float u = Mathf.MoveTowards(rectTransform.rect.width, 0, 150.0f);
+            rectTransform.sizeDelta = new Vector2(Mathf.Clamp(u, 0.0f, ImageCenterWidth), ImageCenterHeight);
+            if (rectTransform.rect.width == 0)
+            {
+                newVideo = true;
+                change = false;
+
+                Change();
+               
+                video_player_center.Play();
+                video_player_center.Pause();
+
+                if (rectTransform.pivot.x == 1)
+                {
+                    rectTransform.pivot = new Vector2(0, 0.5f);
+                    rectTransform.anchoredPosition = new Vector3(-ImageCenterWidth / 2, 40, 0);
+                }
+                else
+                {
+                    rectTransform.pivot = new Vector2(1, 0.5f);
+                    rectTransform.anchoredPosition = new Vector3(ImageCenterWidth / 2, 40, 0);
+                }
+            }
+        }
+        if (newVideo)
+        {
+            float u = Mathf.MoveTowards(rectTransform.rect.width, ImageCenterWidth, 150.0f);
+            rectTransform.sizeDelta = new Vector2(Mathf.Clamp(u, 0.0f, ImageCenterWidth), ImageCenterHeight);
+            if (rectTransform.rect.width == ImageCenterWidth)
+            {
+                newVideo = false;
+                video_player_center.Play();
+                audio_source_center.Play();
+            }
+        }
+        else
+        {
+            if (CursorRight.HandGreen)
+            {
+                if (!RbInicio)
+                {
+                    Rinicio = CursorRight.transform.position.x;
+                    RbInicio = true;
+                }
+            }
+            else if (!CursorRight.HandGreen)
+            {
+                if (RbInicio)
+                {
+                    Rfin = CursorRight.transform.position.x;
+                    if (Rinicio - Rfin > 100)
+                    {
+                        video_player_center.Stop();
+                        audio_source_center.Stop();
+                        change = true;
+                        rectTransform.pivot = new Vector2(0, 0.5f);
+                        rectTransform.anchoredPosition = new Vector3(-ImageCenterWidth / 2, 40, 0);
+                        Change_Next();
+                    }
+                    RbInicio = false;
+                }
+            }
+            if (CursorLeft.HandGreen)
+            {
+                if (!LbInicio)
+                {
+                    Linicio = CursorLeft.transform.position.x;
+                    LbInicio = true;
+                }
+            }
+            else if (!CursorLeft.HandGreen)
+            {
+                if (LbInicio)
+                {
+                    Lfin = CursorLeft.transform.position.x;
+                    if (Lfin - Linicio > 100)
+                    {
+                        video_player_center.Stop();
+                        audio_source_center.Stop();
+                        change = true;
+                        rectTransform.pivot = new Vector2(1, 0.5f);
+                        rectTransform.anchoredPosition = new Vector3(ImageCenterWidth / 2, 40, 0);
+                        Change_Before();
+                    }
+                    LbInicio = false;
+                }
+            }
+        }
+    }
+
+    void CancelTutorial()
+    {
+        if (timeTutorial <= 0)
+        {
+            HandRightTutorial.enabled = false;
+            HandLeftTutorial.enabled = false;
+        }
+        else
+        {
+            timeTutorial -= Time.deltaTime;
+        }
 
     }
 
     public void Update_All()
     {
-        widthLeft = imageLeft.rectTransform.rect.width;
-        heightLeft = imageLeft.rectTransform.rect.height;
-        widthRight = imageRight.rectTransform.rect.width;
-        heightRight = imageRight.rectTransform.rect.height;
-        widthCenter = imageCenter.rectTransform.rect.width;
-        heightCenter = imageCenter.rectTransform.rect.height;
-        posXLeft = imageLeft.transform.position.x;
-        posYLeft = imageLeft.transform.position.y;
-        posXRight = imageRight.transform.position.x;
-        posYRight = imageRight.transform.position.y;
-        posXCenter = imageCenter.transform.position.x;
-        posYCenter = imageCenter.transform.position.y;
-
         btnPosX = BtnStart.transform.position.x;
         btnPosY = BtnStart.transform.position.y;
     }
@@ -184,12 +215,11 @@ public class ChangeVideo : MonoBehaviour
     {
         if (contador < maxContador)
         {
-            video_player_center.Stop();
-            audio_source_center.Stop();
             contador++;
-            Change();
-            video_player_center.Play();
-            audio_source_center.Play();
+        }
+        else if (contador == maxContador)
+        {
+            contador = 1;
         }
     }
 
@@ -197,12 +227,11 @@ public class ChangeVideo : MonoBehaviour
     {
         if (contador > 1)
         {
-            video_player_center.Stop();
-            audio_source_center.Stop();
             contador--;
-            Change();
-            video_player_center.Play();
-            audio_source_center.Play();
+        }
+        else if (contador == 1)
+        {
+            contador = maxContador;
         }
     }
     public void Click_Start()
@@ -229,58 +258,29 @@ public class ChangeVideo : MonoBehaviour
     {
         if (contador == 1)
         {
-            rt1.Release();
-            video_player_left.clip = null;
             video_player_center.clip = v1.clip;
-            video_player_right.clip = v2.clip;
-            video_player_right.Play();
-            video_player_right.Pause();
             audio_source_center = m1;
 
         }
         else if (contador == 2)
         {
-            video_player_left.clip = v1.clip;
-            video_player_left.Play();
-            video_player_left.Pause();
             video_player_center.clip = v2.clip;
-            video_player_right.clip = v3.clip;
-            video_player_right.Play();
-            video_player_right.Pause();
             audio_source_center = m2;
         }
         else if (contador == 3)
         {
-            video_player_left.clip = v2.clip;
-            video_player_left.Play();
-            video_player_left.Pause();
             video_player_center.clip = v3.clip;
-            video_player_right.clip = v4.clip;
-            video_player_right.Play();
-            video_player_right.Pause();
             audio_source_center = m3;
         }
         else if (contador == 4)
         {
-            video_player_left.clip = v3.clip;
-            video_player_left.Play();
-            video_player_left.Pause();
             video_player_center.clip = v4.clip;
-            video_player_right.clip = v5.clip;
-            video_player_right.Play();
-            video_player_right.Pause();
             audio_source_center = m4;
         }
         else if (contador == 5)
         {
-            video_player_left.clip = v4.clip;
-            video_player_left.Play();
-            video_player_left.Pause();
             video_player_center.clip = v5.clip;
-            video_player_right.clip = null;
             audio_source_center = m5;
-
-            rt3.Release();
         }
     }
 }
