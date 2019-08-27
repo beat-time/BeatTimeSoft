b2273f70f11ab0c8393cf7141ed7e6102437d3ca
EielsonJ <42283903+EielsonJ@users.noreply.github.com>
2019-08-26 20:41:14 -0500

    Ventana Emergente cuando el usuario está inactivo
    Si el usuario no está activo fente al Kinect durante 5 segundos aparece una ventana emergente con un contador en descenso de 5 segundos. Si el usuario no vuelve a estar activo durante dicho tiempo, se le retorna a la escena principal, caso contrario, se cierra la venta emergente.

--- BeatTimeBeta/Assets/ActiveUserController.cs
+++ BeatTimeBeta/Assets/ActiveUserController.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ActiveUserController : MonoBehaviour
 {
     public BodySourceView bodySourceView;
     public float time;
     float totalTime = 10.0f;
+    public Canvas canvasAlert;
+    public Text txt_number;
     // Start is called before the first frame update
     void Start()
     {
         time = totalTime;
+        canvasAlert.enabled = false;
+        txt_number.text = totalTime.ToString("f0");
     }
 
     // Update is called once per frame
@@ -24,10 +29,16 @@ public class ActiveUserController : MonoBehaviour
             {
                 SceneManager.LoadScene("MainScene");
             }
+            else if (time <= totalTime / 2)
+            {
+                canvasAlert.enabled = true;
+            }
+            txt_number.text = time.ToString("f0");
         }
         else
         {
             time = totalTime;
+            canvasAlert.enabled = false;
         }
     }
 }
