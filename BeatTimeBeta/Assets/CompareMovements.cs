69635d01e2dc3f608d873c92d1f83aa6c45390f9
EielsonJ <42283903+EielsonJ@users.noreply.github.com>
2019-10-09 12:38:47 -0500

    Mejoramiento de la comparación de movimientos
    Se mejoró la comparación de movimientos entre el coreógrafo virtual y el jugador.

--- BeatTimeBeta/Assets/CompareMovements.cs
+++ BeatTimeBeta/Assets/CompareMovements.cs
@@ -16,9 +16,10 @@ public class CompareMovements : MonoBehaviour
     List<Vector3> JointsMachine = new List<Vector3>();
     int cont = 0;
     int numberOfMovements = 0;
-    float dif1 = 2f;
-    float dif2 = 3f;
-    float dif3 = 5f;
+    int movementsPerSecond = 0;
+    float dif1 = 1f;
+    float dif2 = 2f;
+    float dif3 = 3f;
     float xPlayer = 0;
     float yPlayer = 0;
     float zPlayer = 0;
@@ -43,12 +44,13 @@ public class CompareMovements : MonoBehaviour
     string qualification = "";
     // Start is called before the first frame update
     void Start()
-    {       
+    {
         ReadMovements leerMovimientos = new ReadMovements();
         positions = leerMovimientos.LoadData();
         numberOfMovements = positions.Count / 28;
         cont = numberOfMovements;
         maxTimeCompare = timeCompare;
+        movementsPerSecond = positions.Count / 56;
     }
 
     // Update is called once per frame
@@ -69,71 +71,93 @@ public class CompareMovements : MonoBehaviour
                 if (bodyObject != null)
                 {
                     JointsPlayer = GetJoints(bodyObject);
-                    JointsMachine = positions[cont];
 
-                    for (int a = 0; a < numberOfJoints; a++)
+                    int inicio = numberOfMovements - movementsPerSecond;
+                    int max = numberOfMovements + movementsPerSecond;
+                    int auxPoints = 0;
+
+                    if (inicio < 1)
                     {
-                        xPlayer = JointsPlayer[a].x;
-                        yPlayer = JointsPlayer[a].y;
-                        zPlayer = JointsPlayer[a].z;
+                        inicio = 1;
+                    }
+                    if (max > positions.Count)
+                    {
+                        max = positions.Count;
+                    }
 
-                        xMachine = JointsMachine[a].x;
-                        yMachine = JointsMachine[a].y;
-                        zMachine = JointsMachine[a].z;
+                    while (inicio <= max)
+                    {
+                        auxPoints = 0;
+                        JointsMachine = positions[inicio];
+                        for (int b = 0; b < numberOfJoints; b++)
+                        {
+                            xPlayer = JointsPlayer[b].x;
+                            yPlayer = JointsPlayer[b].y;
+                            zPlayer = JointsPlayer[b].z;
 
-                        if (a == 0)
-                        {
-                            xPlayerAux = xPlayer;
-                            yPlayerAux = yPlayer;
-                            zPlayerAux = zPlayer;
+                            xMachine = JointsMachine[b].x;
+                            yMachine = JointsMachine[b].y;
+                            zMachine = JointsMachine[b].z;
 
-                            xPlayer = 0;
-                            yPlayer = 0;
-                            zPlayer = 0;
-                        }
-                        else
-                        {
-                            Vector3 v = new Vector3(xPlayer, yPlayer, zPlayer);
-                            v = TransformJoint(v, new Vector3(xPlayerAux, yPlayerAux, zPlayerAux));
+                            if (b == 0)
+                            {
+                                xPlayerAux = xPlayer;
+                                yPlayerAux = yPlayer;
+                                zPlayerAux = zPlayer;
 
-                            xPlayer = v.x;
-                            yPlayer = v.y;
-                            zPlayer = v.z;
-                           
+                                xPlayer = 0;
+                                yPlayer = 0;
+                                zPlayer = 0;
+                            }
+                            else
+                            {
+                                Vector3 v = new Vector3(xPlayer, yPlayer, zPlayer);
+                                v = TransformJoint(v, new Vector3(xPlayerAux, yPlayerAux, zPlayerAux));
+
+                                xPlayer = v.x;
+                                yPlayer = v.y;
+                                zPlayer = v.z;
+
+                            }
+                            //float distanceBefore = Vector3.Distance(JointsPlayer[a], JointsMachine[a]);
+                            float distance = Vector3.Distance(new Vector3(xPlayer, yPlayer, zPlayer), new Vector3(xMachine, yMachine, zMachine));
+                            //float distanceAfter = Vector3.Distance(JointsPlayer[a], JointsMachine[a]);
+                            if (distance <= dif1)
+                            {
+                                auxPoints += 10;
+                            }
+                            else if (distance <= dif2)
+                            {
+                                auxPoints += 5;
+                            }
+                            else if (distance <= dif3)
+                            {
+                                auxPoints += 2;
+                            }
+                            /*if (xPlayer >= xMachine - dif1 && xPlayer <= xMachine + dif1 &&
+                                 yPlayer >= yMachine - dif1 && yPlayer <= yMachine + dif1 &&
+                                 zPlayer >= zMachine - dif1 && zPlayer <= zMachine + dif1)
+                             {
+                                 points += 10;
+                             }
+                             else if (xPlayer >= xMachine - dif2 && xPlayer <= xMachine + dif2 &&
+                                 yPlayer >= yMachine - dif2 && yPlayer <= yMachine + dif2 &&
+                                 zPlayer >= zMachine - dif2 && zPlayer <= zMachine + dif2)
+                             {
+                                 points += 5;
+                             }
+                             else if (xPlayer >= xMachine - dif3 && xPlayer <= xMachine + dif3 &&
+                                 yPlayer >= yMachine - dif3 && yPlayer <= yMachine + dif3 &&
+                                 zPlayer >= zMachine - dif3 && zPlayer <= zMachine + dif3)
+                             {
+                                 points += 2;
+                             }*/
                         }
-                        //float distanceBefore = Vector3.Distance(JointsPlayer[a], JointsMachine[a]);
-                        float distance = Vector3.Distance(new Vector3(xPlayer,yPlayer,zPlayer), JointsMachine[a]);
-                        //float distanceAfter = Vector3.Distance(JointsPlayer[a], JointsMachine[a]);
-                        if (distance <= dif1)
+                        if (auxPoints > points)
                         {
-                            points += 10;
+                            points = auxPoints;
                         }
-                        else if (distance <= dif2)
-                        {
-                            points += 5;
-                        }
-                        else if (distance <= dif3)
-                        {
-                            points += 2;
-                        }
-                        /*if (xPlayer >= xMachine - dif1 && xPlayer <= xMachine + dif1 &&
-                            yPlayer >= yMachine - dif1 && yPlayer <= yMachine + dif1 &&
-                            zPlayer >= zMachine - dif1 && zPlayer <= zMachine + dif1)
-                        {
-                            points += 10;
-                        }
-                        else if (xPlayer >= xMachine - dif2 && xPlayer <= xMachine + dif2 &&
-                            yPlayer >= yMachine - dif2 && yPlayer <= yMachine + dif2 &&
-                            zPlayer >= zMachine - dif2 && zPlayer <= zMachine + dif2)
-                        {
-                            points += 5;
-                        }
-                        else if (xPlayer >= xMachine - dif3 && xPlayer <= xMachine + dif3 &&
-                            yPlayer >= yMachine - dif3 && yPlayer <= yMachine + dif3 &&
-                            zPlayer >= zMachine - dif3 && zPlayer <= zMachine + dif3)
-                        {
-                            points += 2;
-                        }*/
+                        inicio += 2;
                     }
                 }
                 if (points == 250)
@@ -144,7 +168,7 @@ public class CompareMovements : MonoBehaviour
                 {
                     qualification = "Good!!!";
                 }
-                else if (points >= 100 && points < 200)
+                else if (points >= 150 && points < 200)
                 {
                     qualification = "Regular";
                 }
