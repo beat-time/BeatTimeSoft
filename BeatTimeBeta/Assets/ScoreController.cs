ddaa7cde1d7f1621496658d0aba7c9b163259e54
EielsonJ <42283903+EielsonJ@users.noreply.github.com>
2019-10-15 16:34:42 -0500

    Se actualizó el script "ScoreController"
    Se eliminó código que no valía.

--- BeatTimeBeta/Assets/ScoreController.cs
+++ BeatTimeBeta/Assets/ScoreController.cs
@@ -8,35 +8,8 @@ using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour
 {
-    public KinectUICursor CursorRight;
-    public KinectUICursor CursorLeft;
-
     public Image photo;
     public Text txt_score;
-
-    public Button btnRepeatDance;
-    public Button btnChooseSong;
-    public Button btnChangePlayers;
-
-    float crHeight;
-    float crWidth;
-    float clHeight;
-    float clWidth;
-
-    float btnRDWidth = 0.0f;
-    float btnRDHeight = 0.0f;
-    float btnRDPosX = 0.0f;
-    float btnRDPosY = 0.0f;
-
-    float btnCSWidth = 0.0f;
-    float btnCSHeight = 0.0f;
-    float btnCSPosX = 0.0f;
-    float btnCSPosY = 0.0f;
-
-    float btnCPWidth = 0.0f;
-    float btnCPHeight = 0.0f;
-    float btnCPPosX = 0.0f;
-    float btnCPPosY = 0.0f;
 
     int totalPoints = 0;
 
@@ -47,36 +20,11 @@ public class ScoreController : MonoBehaviour
         LoadPhoto();
         LoadScore();
         SaveRanking();
-        crHeight = CursorRight.GetComponent<RectTransform>().rect.height;
-        crWidth = CursorRight.GetComponent<RectTransform>().rect.width;
-        clHeight = CursorLeft.GetComponent<RectTransform>().rect.height;
-        clWidth = CursorLeft.GetComponent<RectTransform>().rect.width;
-
-        btnRDWidth = btnRepeatDance.GetComponent<RectTransform>().rect.width;
-        btnRDHeight = btnRepeatDance.GetComponent<RectTransform>().rect.width;
-        btnRDPosX = btnRepeatDance.transform.position.x;
-        btnRDPosY = btnRepeatDance.transform.position.y;
-
-        btnCSWidth = btnChooseSong.GetComponent<RectTransform>().rect.width;
-        btnCSHeight = btnChooseSong.GetComponent<RectTransform>().rect.width;
-        btnCSPosX = btnChooseSong.transform.position.x;
-        btnCSPosY = btnChooseSong.transform.position.y;
-
-        btnCPWidth = btnChangePlayers.GetComponent<RectTransform>().rect.width;
-        btnCPHeight = btnChangePlayers.GetComponent<RectTransform>().rect.width;
-        btnCPPosX = btnChangePlayers.transform.position.x;
-        btnCPPosY = btnChangePlayers.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //UpdatePosition();
-
-        //Click_RepeatDance();
-        //Click_ChooseAnotherSong();
-        //Click_ChangePLayers();
-
         totalTime -= Time.deltaTime;
         if (totalTime < 0)
         {
@@ -89,18 +37,6 @@ public class ScoreController : MonoBehaviour
         CompareMovements compareMovements = new CompareMovements();
         totalPoints = compareMovements.GetTotalPoints();
         txt_score.text = "Puntaje: " + totalPoints.ToString();
-    }
-
-    void UpdatePosition()
-    {
-        btnRDPosX = btnRepeatDance.transform.position.x;
-        btnRDPosY = btnRepeatDance.transform.position.y;
-
-        btnCSPosX = btnChooseSong.transform.position.x;
-        btnCSPosY = btnChooseSong.transform.position.y;
-
-        btnCPPosX = btnChangePlayers.transform.position.x;
-        btnCPPosY = btnChangePlayers.transform.position.y;
     }
     void LoadPhoto()
     {
@@ -129,69 +65,6 @@ public class ScoreController : MonoBehaviour
                 return Tex2D;                 
         }
         return null;                     
-    }
-
-    public void Click_RepeatDance()
-    {
-        if (CursorRight.HandGreen || CursorLeft.HandGreen)
-        {
-            if (CursorRight.transform.position.x - crWidth / 2 >= btnRDPosX - btnRDWidth / 2 &&
-                CursorRight.transform.position.x + crWidth / 2 <= btnRDPosX + btnRDWidth / 2 &&
-                CursorRight.transform.position.y - crHeight / 2 >= btnRDPosY - btnRDHeight / 2 &&
-                CursorRight.transform.position.y + crHeight / 2 <= btnRDPosY + btnRDHeight / 2)
-            {
-                SceneManager.LoadScene("Playing");
-            }
-            else if (CursorLeft.transform.position.x - clWidth / 2 >= btnRDPosX - btnRDWidth / 2 &&
-                CursorLeft.transform.position.x + clWidth / 2 <= btnRDPosX + btnRDWidth / 2 &&
-                CursorLeft.transform.position.y - clHeight / 2 >= btnRDPosY - btnRDHeight / 2 &&
-                CursorLeft.transform.position.y + clHeight / 2 <= btnRDPosY + btnRDHeight / 2)
-            {
-                SceneManager.LoadScene("Playing");
-            }
-        }
-    }
-    
-    public void Click_ChooseAnotherSong()
-    {
-        if (CursorRight.HandGreen || CursorLeft.HandGreen)
-        {
-            if (CursorRight.transform.position.x - crWidth / 2 >= btnCSPosX - btnCSWidth / 2 &&
-                CursorRight.transform.position.x + crWidth / 2 <= btnCSPosX + btnCSWidth / 2 &&
-                CursorRight.transform.position.y - crHeight / 2 >= btnCSPosY - btnCSHeight / 2 &&
-                CursorRight.transform.position.y + crHeight / 2 <= btnCSPosY + btnCSHeight / 2)
-            {
-                SceneManager.LoadScene("SelecThemes");
-            }
-            else if (CursorLeft.transform.position.x - clWidth / 2 >= btnCSPosX - btnCSWidth / 2 &&
-                CursorLeft.transform.position.x + clWidth / 2 <= btnCSPosX + btnCSWidth / 2 &&
-                CursorLeft.transform.position.y - clHeight / 2 >= btnCSPosY - btnCSHeight / 2 &&
-                CursorLeft.transform.position.y + clHeight / 2 <= btnCSPosY + btnCSHeight / 2)
-            {
-                SceneManager.LoadScene("SelecThemes");
-            }
-        }
-    }
-
-    public void Click_ChangePLayers()
-    {
-        if (CursorRight.HandGreen || CursorLeft.HandGreen)
-        {
-            if (CursorRight.transform.position.x - crWidth / 2 >= btnCPPosX - btnCSWidth / 2 &&
-                CursorRight.transform.position.x + crWidth / 2 <= btnCPPosX + btnCSWidth / 2 &&
-                CursorRight.transform.position.y - crHeight / 2 >= btnCPPosY - btnCSHeight / 2 &&
-                CursorRight.transform.position.y + crHeight / 2 <= btnCPPosY + btnCSHeight / 2)
-            {
-                SceneManager.LoadScene("SelecPlayers");
-            }
-            else if (CursorLeft.transform.position.x - clWidth / 2 >= btnCPPosX - btnCPWidth / 2 &&
-                CursorLeft.transform.position.x + clWidth / 2 <= btnCPPosX + btnCPWidth / 2 &&
-                CursorLeft.transform.position.y - clHeight / 2 >= btnCPPosY - btnCPHeight / 2 &&
-                CursorLeft.transform.position.y + clHeight / 2 <= btnCPPosY + btnCPHeight / 2)
-            {
-                SceneManager.LoadScene("SelecPlayers");
-            }
-        }
     }
 
     void SaveRanking()
